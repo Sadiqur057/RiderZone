747dd6277944dff1179d6cf53642f1e336072161
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-01-20 23:47:36 +0600

    customized color using daisy ui

---
+++ script.js
@@ -0,0 +1,12 @@
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'color-primary': '#E76F51',
+        'color-primary-bg': 'rgba(231, 111, 81, 0.10)',
+      }
+    }
+  }
+}
